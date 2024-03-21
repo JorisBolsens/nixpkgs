68b0f8cab836670847d907c0e7219e51f8338f2c
Joris Bolsens <joris@jojolabs.com>
2024-03-21 15:28:53 -0700

    remove if

--- nixos/modules/services/cluster/kubernetes/pki.nix
+++ nixos/modules/services/cluster/kubernetes/pki.nix
@@ -235,7 +235,7 @@ in
                 algo = "rsa";
                 size = 2048;
               };
-              names = mkIf (cert.fields != null) [ cert.fields ];
+              names = [ cert.fields ];
             };
           };
         in
